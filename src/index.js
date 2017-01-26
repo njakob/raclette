d89c2d065d4b115b9ebe38160100401fccc610ca
Nicolas Jakob <nicow.jakob@gmail.com>
2017-01-26 20:14:08 +0100

    Export resolver types

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* @flow */
 
 export { default as resolve } from 'core/resolve';
+export type { ResolveOptions, ResolveEntry, ResolveResult } from 'core/resolve';
 export * as strategies from 'core/strategies';
 export * as loaders from 'core/loaders';
